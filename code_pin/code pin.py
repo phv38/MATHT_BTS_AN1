1dc59d1cb2c08b741e6dec6b581ba14b803a4c5b
phv38 <phv38200@outlook.fr>
2025-01-31 11:19:20 +0100

    123

---
+++ code_pin/code pin.py
@@ -0,0 +1,71 @@
+#   déclaration variable
+
+#def chaine():
+ #   chaine=4321554929
+
+
+
+
+
+#   fonction vérification 4 caractère
+#def verif():
+   
+ #   taille=len(chaine())
+
+  #  if(taille==4):
+   #     print("il y a 4 cara")
+    #else:
+     #   print("pas 4")
+
+#    print(chaine)
+
+
+#verif()
+
+
+#   print pour test
+
+
+
+
+
+
+#chaine1
+
+
+
+
+
+#compteur de chaine
+def Comparaisonchaine(chaine1,chaine2):
+    if(chaine1==chaine2):
+        return True
+    else:
+        return False
+
+def chaine1():
+    chaine=1234
+
+def chaine2():
+    chaine=4321
+
+def vérifcara(chaine1):
+    if(len(chaine1)==4):
+        return True
+    else:
+        return False
+
+def verifchiffre(chaine1,bibli):
+    for i in range(chaine1):
+        if(i not in bibli):
+            return False
+    return True        
+
+
+
+def bibli():
+    bilbio=[0,1,2,3,4,5,6,7,8,9]
+
+
+
+
