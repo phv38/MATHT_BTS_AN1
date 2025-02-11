f3d9fe77211a645eed32d29e72e4da7f56473b8b
phv38 <phv38200@outlook.fr>
2025-02-11 13:23:19 +0100

    code pin avec cesar et boucle

--- code_pin/code pin.py
+++ code_pin/code pin.py
@@ -21,14 +21,37 @@ def verifchiffre(chaine, bibli):
 def bibli():
     return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+def chiffrer_cesar(chaine, cle):
+    resultat = ""
+    for char in chaine:
+        if char.isdigit():
+            resultat += str((int(char) + cle) % 10)
+        else:
+            resultat += char
+    return resultat
+
+def dechiffrer_cesar(chaine, cle):
+    resultat = ""
+    for char in chaine:
+        if char.isdigit():
+            resultat += str((int(char) - cle) % 10)
+        else:
+            resultat += char
+    return resultat
+
 if __name__ == "__main__":
     original = chaine1()
+    cle = 5  # Clé de chiffrement/déchiffrement César
+    
+    original_chiffre = chiffrer_cesar(original, cle)
+    print("Code original chiffré :", original_chiffre)
     
     while True:
         login = chaine2()
         
         if vérifcara(login) and verifchiffre(login, bibli()):
-            if Comparaisonchaine(original, login):
+            login_dechiffre = dechiffrer_cesar(login, cle)
+            if Comparaisonchaine(original, login_dechiffre):
                 print("Codes correspondants")
                 break
             else:
@@ -36,7 +59,6 @@ if __name__ == "__main__":
         else:
             print("Code invalide")
         
-        # Demande si l'utilisateur veut essayer encore une fois
-        retry = input("Voulez-vous essayer encore une fois ? (oui/non): ")
+        retry = input("veux tu réessayer? (oui/non): ")
         if retry.lower() != 'oui':
             break
