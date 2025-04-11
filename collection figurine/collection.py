cee7296dfa391af4238ad1dc4589e298a2d579e8
phv38 <phv38200@outlook.fr>
2025-04-11 11:49:09 +0200

    Update collection.py

--- collection figurine/collection.py
+++ collection figurine/collection.py
@@ -1,18 +1,21 @@
 from random import randint
 
-collection = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+collection = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  #créer la liste
 
+#vérifie si la collection est full
 def verif():
     for i in range(1, 10): 
         if collection[i] == 0:
             return False
     return True
-
+    
+#met a jour la collection de figurine (liste de manière random)
 def ajoute():
     nb_aleatoire = randint(0, 10)  
     collection[nb_aleatoire] += 1 
     print(f"Essai actuel : {collection}")
 
+#permet de tester le nombre de fois qu'il faudrait pour avoir la collection complète
 def test():
     compteur = 0  
     while not verif(): 
